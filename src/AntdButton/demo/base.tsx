bee2f592e68f52deab62870b5f04a24c2a3f4c25
17639638825 <562985990@qq.com>
2024-08-23 10:07:42 +0800

    demo

--- src/AntdButton/demo/base.tsx
+++ src/AntdButton/demo/base.tsx
@@ -4,7 +4,7 @@ export default () => {
   return (
     <>
       <AntButton type="default">默认按钮</AntButton> &nbsp;
-      {/* <Button type="primary">主要按钮</Button> */}
+      <AntButton type="primary">主要按钮</AntButton>
     </>
   );
 };
