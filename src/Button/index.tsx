9e940f46f6d955e16065760ceb19b60f1bfffd38
17639638825 <562985990@qq.com>
2024-08-27 20:26:11 +0800

    button

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,22 +1,36 @@
 import React, { memo } from 'react';
-import './index.less'; // 引入样式
+import styles from './index.less'; // 引入样式
+type ButtonType = 'primary' | 'default' | 'dashed' | 'text' | 'link';
+type ButtonHTMLType = 'submit' | 'button' | 'reset';
 export interface ButtonProps {
   /** 按钮类型 */
-  type?: 'primary' | 'default';
+  type?: ButtonType;
+  htmlType?: ButtonHTMLType;
+  disabled?: boolean;
+  loading?: boolean;
   /** 按钮文字 */
-  children?: React.ReactNode;
+  children: React.ReactNode;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 /** 按钮组件 */
 const Button: React.FC<ButtonProps> = (props) => {
-  const { type = 'default', children, onClick } = props;
+  const {
+    type = 'default',
+    children,
+    onClick,
+    htmlType = 'button',
+    disabled = false,
+    loading = false,
+  } = props;
+  const buttonClass = '';
   return (
     <button
-      type="button"
-      className={`dumi-btn ${type ? 'dumi-btn-' + type : ''}`}
+      type={htmlType}
+      className={buttonClass}
       onClick={onClick}
+      disabled={disabled || loading}
     >
-      {children}
+      {loading ? <span className={styles.spinner}></span> : children}
     </button>
   );
 };
